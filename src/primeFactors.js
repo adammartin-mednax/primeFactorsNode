e6770a9e3dbeddadcd7e6fc98b8478f388f3e260
ajmartin <adam_martin@mednax.net>
2021-09-15 09:54:31 -0400

    Final refactor

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -2,11 +2,10 @@ function primeFactors(suppliedNumber) {
   let number = suppliedNumber;
   let primes = [];
   let candidate = 2;
-  while(number > 1) {
+  for(candidate; number > 1; candidate += 1) {
     for(candidate; number % candidate === 0; number /= candidate) {
       primes.push(candidate);
     }
-    candidate += 1;
   }
   return primes;
 }
